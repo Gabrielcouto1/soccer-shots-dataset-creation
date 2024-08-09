be44491dcad9ebcf0df38e21acee7343ae147f37
Gabriel Couto <gabrielcouto3.14@gmail.com>
2024-08-09 10:53:10 -0300

    feat: added 51 uefa euro matches

--- copy_interested_matches.py
+++ copy_interested_matches.py
@@ -2,31 +2,31 @@ import json
 import os
 import shutil
 
-# Get the desired matches IDs and teams
-file = open("data/copa_america_24_matches.json")
-data = json.load(file)
+data = json.load(open("data/uefa_euro_24_matches.json"))
+
+events_source_folder      = "../StatsBomb-Data/open-data/data/events"
+events_destination_folder = "data/events_uefa_euro_24"
 
 ids    = []
 team1 = []
 team2 = []
 
+# Get the desired matches IDs and teams
 for i in range(len(data)):
     ids.append(data[i]['match_id'])
     team1.append(data[i]['home_team']['home_team_name'])
     team2.append(data[i]['away_team']['away_team_name'])
 
+print(len(ids))
 
 # Move and rename the match files containing each event
-source_folder      = "../StatsBomb-Data/open-data/data/events"
-destination_folder = "data/events_copa_america_24"
-
 for i, file_id in enumerate(ids):
     filename = f"{file_id}.json"
-    source_path = os.path.join(source_folder,filename)
+    source_path = os.path.join(events_source_folder,filename)
 
     if os.path.exists(source_path):
         new_filename     = f"{team1[i]}_vs_{team2[i]}.json"
-        destination_path = os.path.join(destination_folder, new_filename)
+        destination_path = os.path.join(events_destination_folder, new_filename)
 
         shutil.copyfile(source_path, destination_path)
 
