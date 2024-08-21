276e127b2b8eebf21f5d69e1819be31d97216037
Gabriel Couto <gabrielcouto3.14@gmail.com>
2024-08-21 17:10:56 -0300

    feat: modularized the plot with differente dimensions

--- r/plot_empty_field.r
+++ r/plot_empty_field.r
@@ -1,35 +1,47 @@
 library("plotrix")
 
-plot_field <- function() {
-    
-    windows(width=12, height=8)
+plot_field <- function(width, height) {
+    h <- height
+    w <- width
+
+    windows(width=w/10, height=h/10)
     par(bg="green")
-    plot(1, type="n", xlim=c(0,120), ylim=c(0,80))
+    plot(1, type="n", xlim=c(0,w), ylim=c(0,h))
 
-    rect(0, 0, 120, 80, border="white", lwd=2) # Desenhando linhas laterais do campo
+    rect(0, 0, w, h, border="white", lwd=2) # Desenhando linhas laterais do campo
 
-    points(60, 40, col="white", pch=19, cex=1.5) # Ponto do meio de campo
+    points(w/2, h/2, col="white", pch=19, cex=1.5) # Ponto do meio de campo
 
-    segments(60, 0, 60, 80, col="white", lwd=2) # Linha de meio de campo
+    segments(w/2, 0, w/2, h, col="white", lwd=2) # Linha de meio de campo
 
-    draw.circle(60,40,9.15, border="white") # Círculo do meio campo
+    draw.circle(w/2, h/2, 9.15, border="white") # Círculo do meio campo
 
 
-    draw.circle(11,40,9.15, border="white") # Círculo da grande área esquerda
-    rect(0, 19.84, 16.5, 60.16, col="green", border="white") # Grande área esquerda
-    points(11, 40, pch=19, col="white") # Marca do Penalti esquerda
-    rect(0, 30.84, 5.5, 49.16, border="white") # Pequena area esquerda
-    rect(-2.44, 36.34, 0, 43.66, border="white") # Gol esquerdo
+    draw.circle(11,h/2,9.15, border="white") # Círculo da grande área esquerda
+
+    rect(0, ((h/2)-20.16), 16.5, ((h/2)+20.16), col="green", border="white") # Grande área esquerda
     
-    draw.circle(109,40,9.15, border="white") # Círculo da grande área direita
-    rect(120, 19.84, 103.5, 60.16, col="green", border="white") # Grande área direita
-    points(109, 40, pch=19, col="white") # Marca do Penalti direita
-    rect(120, 30.84, 114.5, 49.16, border="white") # Pequena area direita
-    rect(120, 36.34, 122.44, 43.66, border="white") # Gol esquerdo
+    points(11, h/2, pch=19, col="white") # Marca do Penalti esquerda
+
+    rect(0, ((h/2)-9.16), 5.5, ((h/2)+9.16), border="white") # Pequena area esquerda
+
+    rect(-2.44, ((h/2)-3.66), 0, ((h/2)+3.66), border="white") # Gol esquerdo
+    
+
+    
+    draw.circle(w-11, h/2, 9.15, border="white") # Círculo da grande área direita
+
+    rect(w, ((h/2)-20.16), w-16.5, ((h/2)+20.16), col="green", border="white") # Grande área direita
+    
+    points(w-11, h/2, pch=19, col="white") # Marca do Penalti direita
+
+    rect(w, ((h/2)-9.16), w-5.5, ((h/2)+9.16), border="white") # Pequena area direita
+
+    rect(w, ((h/2)-3.66), w+2.44, ((h/2)+3.66), border="white") # Gol esquerdo
 
     # Desenhando marcas dos escanteios
     draw.arc(0, 0, radius = 1, deg1 = 0, deg2 = 90, col = "white", lwd=2) 
-    draw.arc(120, 0, radius = 1, deg1 = 180, deg2 = 90, col = "white", lwd=2)
-    draw.arc(120, 80, radius = 1, deg1 = 180, deg2 = 270, col = "white", lwd=2)
-    draw.arc(0, 80, radius = 1, deg1 = 270, deg2 = 360, col = "white", lwd=2)
+    draw.arc(w, 0, radius = 1, deg1 = 180, deg2 = 90, col = "white", lwd=2)
+    draw.arc(w, h, radius = 1, deg1 = 180, deg2 = 270, col = "white", lwd=2)
+    draw.arc(0, h, radius = 1, deg1 = 270, deg2 = 360, col = "white", lwd=2)
 }
