0e151a34cdb0e46cf8db0cf3aaf145652cd49039
Gabriel Couto <gabrielcouto3.14@gmail.com>
2024-08-21 18:01:47 -0300

    new: plot now saves the image in plots folder

--- r/draw_passes.r
+++ r/draw_passes.r
@@ -3,9 +3,21 @@ source("./r/get_pass_distance.r")
 source("./r/plot_empty_field.r")
 source("./r/get_pass_ids.r")
 
-match_json <- fromJSON(file = "./data/events_copa_america_24/Argentina_vs_Canada.json") 
+match <- "Argentina_vs_Canada"
 
-plot_field()
+json_path  <- paste0("./data/events_copa_america_24/", match)
+json_path  <- paste0(json_path, ".json")
+match_json <- fromJSON(file = json_path) 
+
+img_path <- paste0("./r/plots/", match)
+img_path <- paste0(img_path, ".png")
+
+width  <- match_json[[5]]$location[1]*2
+height <- match_json[[5]]$location[2]*2
+
+png(file=img_path, width=width*10, height=height*10)
+
+plot_field(width, height)
 
 for (i in 1:length(match_json)){
     if((match_json[[i]]$type$id) == 30){
@@ -23,3 +35,6 @@ for (i in 1:length(match_json)){
         segments(source[1], source[2], destination[1], destination[2], col="grey")
     }
 }
+
+mtext(match, side = 1, line = 2)
+dev.off()
