e70f48322473617d3297765b4e0f84d0f660ce26
Gabrielcouto1 <gabrielcouto3.14@gmail.com>
2025-05-20 12:43:53 -0300

    refactor: changes in folder structure and renaming a few files

--- r/utils/get_distance.r
+++ r/utils/get_distance.r
@@ -1,4 +1,4 @@
-get_pass_distance<-function(source, destination){
+get_distance<-function(source, destination){
     distance<-((destination[1]-source[1]))^2 + (destination[2]-source[2])^2
 
     return (sqrt(distance))
