29066256a6f567ac62a776f6164149ad6f40d2d1
Gabrielcouto1 <gabrielcouto3.14@gmail.com>
2025-06-12 10:04:14 -0300

    feat: gauge graph almost complete

--- r/utils/plot/plot_gauge_chart.r
+++ r/utils/plot/plot_gauge_chart.r
@@ -10,11 +10,11 @@ plot_gauge_chart <- function(home_prob, draw_prob, away_prob, home_team, away_te
     posicao_ponteiro <- home_prob
 
     cores <- setNames(c("#00FF00", "#838383", "#FF0000", "white"), 
-                 c(home_team, "Draw", away_team, "Invisível"))
+                 	  c(home_team, "Draw", away_team, "Invisível"))
 
     df <- data.frame(
-      categoria = c(home_team, "Draw", away_team, "Invisível"),
-      valor = c(home_prob, draw_prob, away_prob, 100)
+      	categoria = c(home_team, "Draw", away_team, "Invisível"),
+      	valor = c(home_prob, draw_prob, away_prob, 100)
     )
 
     df$fracao <- df$valor / sum(df$valor)
@@ -23,46 +23,70 @@ plot_gauge_chart <- function(home_prob, draw_prob, away_prob, home_team, away_te
 
     # Create just the gauge chart without any annotations
     gauge_chart <- ggplot() +
-      geom_rect(data = df, aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = categoria)) +
-      geom_segment(
-        aes(x = 2.5, y = posicao_ponteiro / 200, xend = 3.8, yend = posicao_ponteiro / 200),
-        arrow = arrow(length = unit(0.3, "cm")),
-        lineend = "round",
-        linewidth = 1.5,
-        color = "black"
-      ) +
-      coord_polar(theta = "y", start = -pi/2) +
-      xlim(c(0, 4)) +
-      scale_fill_manual(values = cores) +
-      annotate(
-        "text", x = 0, y = 0,
-        label = paste0(round(posicao_ponteiro, 1), "%"),
-        size = 12,
-        fontface = "bold",
-        color = "black"
-      ) +
-      theme_void() +
-      theme(legend.position = "none") +
-      labs(title = "Índice de merecimento") +
-      theme(plot.title = element_text(hjust = 0.5, size = 16, face = "bold"))
+      	geom_rect(data = df, aes(ymax = ymax, ymin = ymin, xmax = 4, xmin = 3, fill = categoria)) +
+      	geom_segment(
+      	  aes(x = 2.5, y = posicao_ponteiro / 200, xend = 3.8, yend = posicao_ponteiro / 200),
+      	  arrow = arrow(length = unit(0.3, "cm")),
+      	  lineend = "round",
+      	  linewidth = 1.5,
+      	  color = "black"
+      	) +
+      	coord_polar(theta = "y", start = -pi/2) +
+      	xlim(c(0, 4)) +
+      	scale_fill_manual(values = cores) +
+      	annotate(
+      	  "text", x = 0, y = 0,
+      	  label = paste(round(posicao_ponteiro, 1), "%", sep=""),
+      	  size = 12,
+      	  fontface = "bold",
+      	  color = "black"
+      	) +
+      	theme_void() +
+      	theme(legend.position = "none") +
+      	labs(title = "Outcome simulator (by xG)") +
+      	theme(plot.title = element_text(hjust = 0.5, size = 16, face = "bold"))
 
-    # Create text grobs for the bottom text
-    left_text <- textGrob(
-      paste(home_team, "probability:", round(home_prob, 1), "%"),
-      x = 0.1, y = 2, just = "left", gp = gpar(fontsize = 10))
-    # ARRUMAR ESSA MERDA AQ PQP!!!!!!!!!!!!!!!!!!
-    center_text <- textGrob(
-      paste("Draw probability:", round(draw_prob, 1), "%"),
-      x = 0.5, y = 0.5, just = "left", gp = gpar(fontsize = 10))
+	outcome_text <- textGrob(
+        paste(home_team, " (placeholder) ", away_team, sep=""),
+        x = 2, y = 3, just = "centre", gp = gpar(fontsize = 20))
+
+    home_prob_text <- textGrob(
+        paste(home_team, " wins ", round(home_prob, 1), "% of times", sep=""),
+        x = 0.1, y = 2.5, just = "centre", gp = gpar(fontsize = 13))
     
-    right_text <- textGrob(
-      paste(away_team, "probability:", round(away_prob, 1), "%"),
-      x = 19, y = 0.5, just = "right", gp = gpar(fontsize = 10))
+    away_prob_text <- textGrob(
+        paste(away_team, " wins ", round(away_prob, 1), "% of times", sep=""),
+        x = 1, y = 2.5, just = "centre", gp = gpar(fontsize = 13))
 
-    # Combine everything
+	text <- paste("These numbers are a result of 10.000 simulations of the match.",
+				  "The simulations follow a binomial distribuition, where X is a shot",
+				  "and the probability is statsbomb's xG.",
+				  "This grafic is inspired by CruzeiroData (x.com/CruzeiroData).", sep="\n")
+	
+	description <- textGrob(
+        text,
+        x = -2, y = 1, just = "left", gp = gpar(fontsize = 10))
+
     grid.arrange(
       gauge_chart,
-      arrangeGrob(left_text, center_text, right_text, nrow = 1),
-      heights = c(0.9, 0.1)
+      arrangeGrob(outcome_text, home_prob_text, away_prob_text, description, nrow = 1),
+      heights = c(0.5, 0.1)
     )
 }
+
+
+
+home_team <- "Argentina"
+away_team <- "Canada"
+match <- paste0(home_team, "_vs_", away_team)
+
+match_xg <- get_match_xg(match)
+
+home_xg <- match_xg$home_xg
+away_xg <- match_xg$away_xg
+
+outcome_prob <- simulate_match_outcome(home_xg, away_xg)
+
+print(outcome_prob)
+
+plot_gauge_chart(outcome_prob$home_prob, outcome_prob$draw_prob, outcome_prob$away_prob, home_team, away_team)
