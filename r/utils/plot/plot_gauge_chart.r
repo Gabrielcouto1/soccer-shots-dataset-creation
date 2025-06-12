38486d434372dd489d77b3094a96681795b583a8
Gabrielcouto1 <gabrielcouto3.14@gmail.com>
2025-06-12 10:24:50 -0300

    feat: finished the gauge graph

--- r/utils/plot/plot_gauge_chart.r
+++ r/utils/plot/plot_gauge_chart.r
@@ -1,13 +1,15 @@
 library(ggplot2)
 library(gridExtra)
 library(grid)
-
+source("./r/utils/get_match_outcome.r")
 plot_gauge_chart <- function(home_prob, draw_prob, away_prob, home_team, away_team){
     home_prob <- home_prob*100
     draw_prob <- draw_prob*100
     away_prob <- away_prob*100
 
     posicao_ponteiro <- home_prob
+
+	match <- paste0(home_team, "_vs_", away_team)
 
     cores <- setNames(c("#00FF00", "#838383", "#FF0000", "white"), 
                  	  c(home_team, "Draw", away_team, "Invisível"))
@@ -47,25 +49,25 @@ plot_gauge_chart <- function(home_prob, draw_prob, away_prob, home_team, away_te
       	theme(plot.title = element_text(hjust = 0.5, size = 16, face = "bold"))
 
 	outcome_text <- textGrob(
-        paste(home_team, " (placeholder) ", away_team, sep=""),
-        x = 2, y = 3, just = "centre", gp = gpar(fontsize = 20))
+        paste(home_team, " (",get_match_outcome(match), ") ",away_team, sep=""),
+        x = 2, y = 2.8, just = "centre", gp = gpar(fontsize = 25))
 
     home_prob_text <- textGrob(
         paste(home_team, " wins ", round(home_prob, 1), "% of times", sep=""),
-        x = 0.1, y = 2.5, just = "centre", gp = gpar(fontsize = 13))
+        x = 0.1, y = 2, just = "centre", gp = gpar(fontsize = 16))
     
     away_prob_text <- textGrob(
         paste(away_team, " wins ", round(away_prob, 1), "% of times", sep=""),
-        x = 1, y = 2.5, just = "centre", gp = gpar(fontsize = 13))
+        x = 1, y = 2, just = "centre", gp = gpar(fontsize = 16))
 
 	text <- paste("These numbers are a result of 10.000 simulations of the match.",
 				  "The simulations follow a binomial distribuition, where X is a shot",
-				  "and the probability is statsbomb's xG.",
-				  "This grafic is inspired by CruzeiroData (x.com/CruzeiroData).", sep="\n")
+				  "and the probability is statsbomb's xG. This graphic was inspired",
+				  "by CruzeiroData (x.com/CruzeiroData).", sep="\n")
 	
 	description <- textGrob(
         text,
-        x = -2, y = 1, just = "left", gp = gpar(fontsize = 10))
+        x = -2, y = 0.35, just = "left", gp = gpar(fontsize = 11))
 
     grid.arrange(
       gauge_chart,
@@ -73,20 +75,3 @@ plot_gauge_chart <- function(home_prob, draw_prob, away_prob, home_team, away_te
       heights = c(0.5, 0.1)
     )
 }
-
-
-
-home_team <- "Argentina"
-away_team <- "Canada"
-match <- paste0(home_team, "_vs_", away_team)
-
-match_xg <- get_match_xg(match)
-
-home_xg <- match_xg$home_xg
-away_xg <- match_xg$away_xg
-
-outcome_prob <- simulate_match_outcome(home_xg, away_xg)
-
-print(outcome_prob)
-
-plot_gauge_chart(outcome_prob$home_prob, outcome_prob$draw_prob, outcome_prob$away_prob, home_team, away_team)
