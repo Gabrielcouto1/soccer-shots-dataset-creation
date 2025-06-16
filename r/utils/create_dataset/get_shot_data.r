946d2858aafdeb9ecb2667a1568c80367af067a7
Gabrielcouto1 <gabrielcouto3.14@gmail.com>
2025-06-16 20:16:01 -0300

    feat: created a function to calculate the angle between the place where the shot was taken and the goal. MUST TEST IF ITS WORKING OKgit add .git add .git add .git add .

--- r/utils/create_dataset/get_shot_data.r
+++ r/utils/create_dataset/get_shot_data.r
@@ -1,6 +1,7 @@
 library("rjson") 
 
 source("./r/utils/plot/get_distance.r")
+source("./r/utils/create_dataset/get_goal_angle.r")
 
 get_shot_data <- function(shot){
     source      <- shot$location
@@ -9,8 +10,14 @@ get_shot_data <- function(shot){
     shot_length   <- get_distance(source, destination)
     goal_distance <- get_distance(source, c(120,40))
 
+    goal_angle <- get_goal_angle(source)
+
+
+
+
     return(list(source=source, 
                 destination=destination,    
                 shot_length=shot_length,
-                goal_distance=goal_distance))
+                goal_distance=goal_distance,
+                goal_angle=goal_angle))
 }
