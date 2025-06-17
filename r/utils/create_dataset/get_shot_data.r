d5494eb49024bbb1de185aed0edb48e599af0ee2
Gabrielcouto1 <gabrielcouto3.14@gmail.com>
2025-06-17 17:28:06 -0300

    feat: implemented some freeze_frame extraction functions

--- r/utils/create_dataset/get_shot_data.r
+++ r/utils/create_dataset/get_shot_data.r
@@ -2,6 +2,7 @@ library("rjson")
 
 source("./r/utils/plot/get_distance.r")
 source("./r/utils/create_dataset/get_goal_angle.r")
+source("./r/utils/create_dataset/freeze_frame.r")
 
 get_shot_data <- function(shot){
     source      <- shot$location
@@ -9,19 +10,21 @@ get_shot_data <- function(shot){
     
     shot_length   <- get_distance(source, destination)
     goal_distance <- get_distance(source, c(120,40))
+    goal_angle    <- get_goal_angle(source)
 
-    goal_angle <- get_goal_angle(source)
-
-    body_part <- shot$shot$body_part$id
+    body_part      <- shot$shot$body_part$id
     body_part_name <- shot$shot$body_part$name
 
-    technique <- shot$shot$technique$id
+    technique      <- shot$shot$technique$id
     technique_name <- shot$shot$technique$name
 
-    type <- shot$shot$type$id
+    type      <- shot$shot$type$id
     type_name <- shot$shot$type$name
 
-    
+    teammate_count <- get_teammate_count(shot)
+    enemy_count    <- get_enemy_count(shot)
+
+    closes_enemy_distance <- get_closest_enemy_distance(shot)
 
     return(list(shot_length=shot_length,
                 goal_distance=goal_distance,
@@ -31,5 +34,8 @@ get_shot_data <- function(shot){
                 technique=technique,
                 technique_name=technique_name, 
                 type=type,
-                type_name=type_name))
+                type_name=type_name,
+                teammate_count=teammate_count,
+                enemy_count=enemy_count,
+                closes_enemy_distance=closes_enemy_distance))
 }
