f768d5321dda765876dabf010234e3bb77574b15
Gabrielcouto1 <gabrielcouto3.14@gmail.com>
2025-06-17 14:51:32 -0300

    new: added a draft of all functions i will need to manipulate the freeze_frame

---
+++ r/utils/create_dataset/freeze_frame.r
@@ -0,0 +1,29 @@
+source("./r/plot/get_distance")
+
+get_teammate_count <- function(shot){
+
+}
+
+get_enemy_count <- function(shot){
+    
+}
+
+get_enemies_count_in_goal_area <- function(shot){
+
+}
+
+get_enemies_count_in_penalty_area <- function(shot){
+
+}
+
+get_enemies_in_ball_trajectory <-function(shot){
+
+}
+
+get_closest_enemy_distance <- function(shot){
+
+}
+
+get_goalkeeper_distance <- function(shot){
+
+}
