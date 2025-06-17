3ce1c324f158321da775b798f94910acbc7aaf59
Gabrielcouto1 <gabrielcouto3.14@gmail.com>
2025-06-17 20:26:30 -0300

    feat: changed the function to create a dataframe to get all shots from a match. TODO: change (or use this code) to get all shots from all matches from a said folder

--- r/create_dataset.r
+++ r/create_dataset.r
@@ -1,4 +1,5 @@
 library("rjson") 
+options("width"=200)
 
 source("./r/utils/create_dataset/get_shot_data.r")
 source("./r/utils/get_match_json.r")
@@ -6,12 +7,18 @@ source("./r/utils/get_match_json.r")
 match_json = get_match_json("Argentina", "Canada", 1)
 
 play_type <- c(16) # 16 = shot 30 = pass
+all_shots_data <- list()
 
 for (i in 1:length(match_json)){
-    current_play_type <- (match_json[[i]]$type$id)
+    current_play <- match_json[[i]]
 
-    if(current_play_type == play_type ){ 
-        print(get_shot_data(match_json[[i]]))
-        break
+    if(!is.null(current_play$type$id) && current_play$type$id == play_type ){ 
+        shot_data <- get_shot_data(current_play)
+        all_shots_data[[length(all_shots_data) + 1]] <- shot_data
     }
 }
+
+shots_df <- do.call(rbind, lapply(all_shots_data, as.data.frame))
+
+print(head(shots_df))
+str(shots_df)
--- r/utils/create_dataset/get_shot_data.r
+++ r/utils/create_dataset/get_shot_data.r
@@ -5,57 +5,48 @@ source("./r/utils/create_dataset/get_goal_angle.r")
 source("./r/utils/create_dataset/freeze_frame.r")
 
 get_shot_data <- function(shot){
-    source      <- shot$location
-    destination <- shot$shot$end_location
+    source       <- shot$location
+    destination  <- shot$shot$end_location
+    shot_dist    <- get_distance(source, destination)
+    dist_to_goal <- get_distance(source, c(120,40))
+    shot_angle   <- get_goal_angle(source)
+
+    shot_technique <- shot$shot$technique$name
+    body_part      <- shot$shot$body_part$name
+    shot_type      <- shot$shot$type$name
+
+    teammates_in_frame <- get_teammate_count(shot)
+    opponents_in_frame <- get_enemy_count(shot)
+
+    closest_opponent_dist <- get_closest_enemy_distance(shot)
     
-    shot_length   <- get_distance(source, destination)
-    goal_distance <- get_distance(source, c(120,40))
-    goal_angle    <- get_goal_angle(source)
-
-    body_part      <- shot$shot$body_part$id
-    body_part_name <- shot$shot$body_part$name
-
-    technique      <- shot$shot$technique$id
-    technique_name <- shot$shot$technique$name
-
-    type      <- shot$shot$type$id
-    type_name <- shot$shot$type$name
-
-    teammate_count <- get_teammate_count(shot)
-    enemy_count    <- get_enemy_count(shot)
-
-    closets_enemy_distance <- get_closest_enemy_distance(shot)
-    
-    gk_distance <- get_goalkeeper_distance(shot)
+    goalkeeper_dist <- get_goalkeeper_distance(shot)
 
     enemies_in_area <- get_enemies_count_in_area(shot)
 
-    penalty_area_count <- enemies_in_area$penalty_area_count
-    goal_area_count <- enemies_in_area$goal_area_count
+    opponents_in_penalty_area <- enemies_in_area$penalty_area_count
+    opponents_in_goal_area <- enemies_in_area$goal_area_count
 
-    enemies_in_trajectory <- get_enemies_in_ball_trajectory(shot)
+    opponents_in_shot_path <- get_enemies_in_ball_trajectory(shot)
 
     if(shot$shot$outcome$id==97){
-        goal <- 1
+        is_goal <- 1
     }else{
-        goal <- 0
+        is_goal <- 0
     }
 
-    return(list(shot_length=shot_length,
-                goal_distance=goal_distance,
-                goal_angle=goal_angle,
+    return(list(shot_dist=shot_dist,
+                dist_to_goal=dist_to_goal,
+                shot_angle=shot_angle,
                 body_part=body_part,
-                body_part_name=body_part_name,
-                technique=technique,
-                technique_name=technique_name, 
-                type=type,
-                type_name=type_name,
-                teammate_count=teammate_count,
-                enemy_count=enemy_count,
-                closets_enemy_distance=closets_enemy_distance,
-                gk_distance=gk_distance,
-                penalty_area_count=penalty_area_count,
-                goal_area_count=goal_area_count,
-                enemies_in_trajectory=enemies_in_trajectory,
-                goal=goal))
+                shot_technique=shot_technique, 
+                shot_type=shot_type,
+                teammates_in_frame=teammates_in_frame,
+                opponents_in_frame=opponents_in_frame,
+                closest_opponent_dist=closest_opponent_dist,
+                goalkeeper_dist=goalkeeper_dist,
+                opponents_in_penalty_area=opponents_in_penalty_area,
+                opponents_in_goal_area=opponents_in_goal_area,
+                opponents_in_shot_path=opponents_in_shot_path,
+                is_goal=is_goal))
 }
