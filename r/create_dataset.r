836012a2eac6c86fc68dc3151dc2413280c32ef1
Gabrielcouto1 <gabrielcouto3.14@gmail.com>
2025-06-19 20:41:51 -0300

    feat: added checks to see if the required libraries are installed

--- r/create_dataset.r
+++ r/create_dataset.r
@@ -1,3 +1,23 @@
+required_packages <- c(
+  "rjson", 
+  "dplyr", 
+  "tidyverse", 
+  "plotrix", 
+  "ggplot2", 
+  "gridExtra", 
+  "grid",
+  "purrr"
+)
+
+for (pkg in required_packages) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    message("Installing missing package: ", pkg)
+    install.packages(pkg, dependencies = TRUE)
+  } else {
+    message(pkg, " is already installed")
+  }
+}
+
 library("rjson") 
 library("dplyr")
 library("tidyverse")
