69053eb515cd014d5c7d93709189250aa43d5d5f
Gabrielcouto1 <gabrielcouto3.14@gmail.com>
2025-06-19 16:29:23 -0300

    refactor: changed locations to ease reproduction

--- python/copy_interested_matches.py
+++ python/copy_interested_matches.py
@@ -1,59 +1,51 @@
 import json
 import os
-import shutil
 
-competition = "bundesliga"
-competition_name = "Bundesliga"
+competitions = ["bundesliga", "la_liga", "ligue_1", "premier_league", "serie_a"]
+competition_names = ["Bundesliga", "La Liga", "Ligue 1", "Premier League", "Serie A"]
 
-# competition = "la_liga"
-# competition_name = "La Liga"
+for i in range(len(competitions)):
 
-# competition = "ligue_1"
-# competition_name = "Ligue 1"
+    competition = competitions[i]
+    competition_name = competition_names[i]
 
-# competition = "premier_league"
-# competition_name = "Premier League"
+    matches_source_folder   = f"../../analise-futebol/data/matches_list/{competition}_matches"
+    events_source_folder    = "../../open-data/data/events"
+    events_destination_folder = f"../data/events/events_{competition}"
 
-# competition = "serie_a"
-# competition_name = "Serie A"
+    os.makedirs(events_destination_folder, exist_ok=True)
 
-matches_source_folder   = f"../../analise-futebol/data/matches_list/{competition}_matches"
-events_source_folder    = "../../StatsBomb-Data/open-data/data/events"
-events_destination_folder = f"../data/events/events_{competition}"
+    for file_name in os.listdir(matches_source_folder):
+        if file_name.endswith('.json'):
+            season = os.path.splitext(file_name)[0]
 
-os.makedirs(events_destination_folder, exist_ok=True)
+            file_path = os.path.join(matches_source_folder, file_name)
 
-for file_name in os.listdir(matches_source_folder):
-    if file_name.endswith('.json'):
-        season = os.path.splitext(file_name)[0]
-        
-        file_path = os.path.join(matches_source_folder, file_name)
-        
-        with open(file_path, 'r', encoding='utf-8') as f:
-            matches_in_season = json.load(f)
-        
-        for match in matches_in_season:
-            match_id = match['match_id']
-            team1 = match['home_team']['home_team_name']
-            team2 = match['away_team']['away_team_name']
+            with open(file_path, 'r', encoding='utf-8') as f:
+                matches_in_season = json.load(f)
 
-            source_event_path = os.path.join(events_source_folder, f"{match_id}.json")
+            for match in matches_in_season:
+                match_id = match['match_id']
+                team1 = match['home_team']['home_team_name']
+                team2 = match['away_team']['away_team_name']
 
-            if os.path.exists(source_event_path):
-                with open(source_event_path, 'r', encoding='utf-8') as f_event:
-                    event_data = json.load(f_event)
-                
-                if isinstance(event_data, list) and len(event_data) > 0:
-                    event_data[0]['competition_name'] = competition_name
-                    event_data[0]['season'] = season
-                    event_data[0]['match_id'] = str(match_id)
-                
-                new_filename = f"{team1}_vs_{team2}_{match_id}.json"
-                destination_path = os.path.join(events_destination_folder, new_filename)
+                source_event_path = os.path.join(events_source_folder, f"{match_id}.json")
 
-                with open(destination_path, 'w', encoding='utf-8') as f_dest:
-                    json.dump(event_data, f_dest, ensure_ascii=False, indent=4)
+                if os.path.exists(source_event_path):
+                    with open(source_event_path, 'r', encoding='utf-8') as f_event:
+                        event_data = json.load(f_event)
 
-                print(f"Processed and moved {match_id}.json to {new_filename}")
-            else:
-                print(f"Event file for match_id {match_id} not found")
+                    if isinstance(event_data, list) and len(event_data) > 0:
+                        event_data[0]['competition_name'] = competition_name
+                        event_data[0]['season'] = season
+                        event_data[0]['match_id'] = str(match_id)
+
+                    new_filename = f"{team1}_vs_{team2}_{match_id}.json"
+                    destination_path = os.path.join(events_destination_folder, new_filename)
+
+                    with open(destination_path, 'w', encoding='utf-8') as f_dest:
+                        json.dump(event_data, f_dest, ensure_ascii=False, indent=4)
+
+                    print(f"Processed and moved {match_id}.json to {new_filename}")
+                else:
+                    print(f"Event file for match_id {match_id} not found")
--- python/get_matches_number.py
+++ python/get_matches_number.py
@@ -1,22 +1,20 @@
 import json
 import os
-import shutil
 
-events_source_folder      = "../..//StatsBomb-Data/open-data/data/events"
+events_source_folder      = "../..//open-data/data/events"
 events_destination_folder = "data/events_la_liga"
 
 import os
 import json
 
 for competition_id in range(1472):
-    matches_source_folder = f"../../StatsBomb-Data/open-data/data/matches/{competition_id}"
+    matches_source_folder = f"../../open-data/data/matches/{competition_id}"
 
-    # Check if the directory exists before proceeding
     if os.path.exists(matches_source_folder) and os.path.isdir(matches_source_folder):
         ids = []
         team1 = []
         team2 = []
-        competition_name = ""  # Initialize competition_name
+        competition_name = ""  
 
         for file_name in os.listdir(matches_source_folder):
             if file_name.endswith('.json'):
@@ -25,8 +23,7 @@ for competition_id in range(1472):
                 with open(file_path, 'r', encoding='utf-8') as f:
                     try:
                         data = json.load(f)
-                        if data:  # Check if the file is not empty
-                            # Safely get competition_name from the first entry if data is a list
+                        if data: 
                             if isinstance(data, list) and data[0]['competition']['competition_name']:
                                 competition_name = data[0]['competition']['competition_name']
 
@@ -39,7 +36,6 @@ for competition_id in range(1472):
                                 ids.append(data['match_id'])
                                 team1.append(data['home_team']['home_team_name'])
                                 team2.append(data['away_team']['away_team_name'])
-                                # Assuming a single match json file might also contain the competition name
                                 if 'competition' in data and 'competition_name' in data['competition']:
                                     competition_name = data['competition']['competition_name']
 
@@ -49,21 +45,6 @@ for competition_id in range(1472):
                         print(f"Warning: Missing expected data in {file_name}: {e}")
 
 
-        if ids:  # Only print if matches were found
+        if ids:  
             print(f"--------Competition {competition_id} ({competition_name})--------")
             print(f"Matches: {len(ids)}")
-
-
-# for i, file_id in enumerate(ids):
-#     filename = f"{file_id}.json"
-#     source_path = os.path.join(events_source_folder,filename)
-
-#     if os.path.exists(source_path):
-#         new_filename     = f"{team1[i]}_vs_{team2[i]}_{file_id}.json"
-#         destination_path = os.path.join(events_destination_folder, new_filename)
-
-#         shutil.copyfile(source_path, destination_path)
-
-#         print(f"Moved {filename} to {new_filename}")
-#     else:
-#         print(f"{ids[i]} not found")
