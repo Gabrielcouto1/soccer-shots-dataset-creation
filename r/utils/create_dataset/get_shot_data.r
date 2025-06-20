2ade39dd011d36b0cbe6687efe9674c3eddb369c
Gabrielcouto1 <gabrielcouto3.14@gmail.com>
2025-06-20 01:37:15 -0300

    refactor: reduced some unecessary code and changed the order of the columns in dataset to make it more coherent and organized

--- r/utils/create_dataset/get_shot_data.r
+++ r/utils/create_dataset/get_shot_data.r
@@ -5,28 +5,35 @@ source("./r/utils/create_dataset/get_goal_angle.r")
 source("./r/utils/create_dataset/freeze_frame.r")
 
 get_shot_data <- function(shot, home_team, away_team, competition_name=competition_name, season=season, match_id=match_id){
+    # Team and Match Context
     attacking_team <- shot$possession_team$name
-
-    if(attacking_team==home_team){
-        defending_team <- away_team
-        is_home        <- 1
-    }else{
-        defending_team <- home_team
-        is_home        <- 0
-    }
-
+    is_home        <- ifelse(attacking_team == home_team, 1, 0)
+    defending_team <- ifelse(is_home == 1, away_team, home_team)
+    
+    # Time and period related characteristics
+    timestamp <- (shot$minute * 60) + shot$second
+    period    <- shot$period
+    
+    # Shot characteristics
     shooter_position <- shot$position$name
-    source           <- shot$location
-    destination      <- shot$shot$end_location
-    shot_dist        <- get_distance(source, destination)
-    dist_to_goal     <- get_distance(source, c(120,40))
-    shot_angle       <- get_goal_angle(source)
-
-    shot_technique <- shot$shot$technique$name
-    body_part      <- shot$shot$body_part$name
-    shot_type      <- shot$shot$type$name
-
-    if(shot$shot$type$id==88){
+    shot_technique   <- shot$shot$technique$name
+    body_part        <- shot$shot$body_part$name
+    shot_type        <- shot$shot$type$name
+    
+    # Spatial and Geometric Features
+    source       <- shot$location
+    destination  <- shot$shot$end_location
+    shot_dist    <- get_distance(source, destination)
+    dist_to_goal <- get_distance(source, c(120, 40))
+    shot_angle   <- get_goal_angle(source)
+    
+    # Situational Flags
+    is_penalty   <- ifelse(shot$shot$type$id == 88, 1, 0)
+    is_ca        <- ifelse(shot$play_pattern$name == "From Counter", 1, 0)
+    is_open_goal <- ifelse(!is.null(shot$shot$open_goal), 1, 0)
+    
+    # Freeze Frame Analysis (player in frame locations)
+    if (is_penalty == 1) {
         teammates_in_frame        <- 0
         opponents_in_frame        <- 0
         closest_opponent_dist     <- 0
@@ -34,62 +41,50 @@ get_shot_data <- function(shot, home_team, away_team, competition_name=competiti
         opponents_in_penalty_area <- 0
         opponents_in_goal_area    <- 0
         opponents_in_shot_path    <- 0
-        is_penalty                <- 1
-    }else{
-        teammates_in_frame <- get_teammate_count(shot)
-        opponents_in_frame <- get_enemy_count(shot)
-
-        closest_opponent_dist <- get_closest_enemy_distance(shot)
-
-        goalkeeper_dist <- get_goalkeeper_distance(shot)
-
-        enemies_in_area <- get_enemies_count_in_area(shot)
-
+    } else {
+        enemies_in_area           <- get_enemies_count_in_area(shot)
+        teammates_in_frame        <- get_teammate_count(shot)
+        opponents_in_frame        <- get_enemy_count(shot)
+        closest_opponent_dist     <- get_closest_enemy_distance(shot)
+        goalkeeper_dist           <- get_goalkeeper_distance(shot)
         opponents_in_penalty_area <- enemies_in_area$penalty_area_count
-        opponents_in_goal_area <- enemies_in_area$goal_area_count
-
-        opponents_in_shot_path <- get_enemies_in_ball_trajectory(shot)
-        is_penalty <- 0
-    }
-
-    if(!is.null(shot$shot$open_goal)){
-        is_open_goal <- 1
-    } else{
-        is_open_goal <- 0
-    }
-
-    statsbomb_xg <- shot$shot$statsbomb_xg
-    outcome      <- shot$shot$outcome$name
-
-    if(shot$shot$outcome$id==97){
-        is_goal <- 1
-    }else{
-        is_goal <- 0
+        opponents_in_goal_area    <- enemies_in_area$goal_area_count
+        opponents_in_shot_path    <- get_enemies_in_ball_trajectory(shot)
     }
     
-    return(list(competition=competition_name,
-                season=season,
-                match_id=match_id,
-                shooter_position=shooter_position,
-                attacking_team=attacking_team,
-                is_home=is_home,
-                defending_team=defending_team,
-                shot_dist=shot_dist,
-                dist_to_goal=dist_to_goal,
-                shot_angle=shot_angle,
-                body_part=body_part,
-                shot_technique=shot_technique, 
-                shot_type=shot_type,
-                teammates_in_frame=teammates_in_frame,
-                opponents_in_frame=opponents_in_frame,
-                closest_opponent_dist=closest_opponent_dist,
-                goalkeeper_dist=goalkeeper_dist,
-                opponents_in_penalty_area=opponents_in_penalty_area,
-                opponents_in_goal_area=opponents_in_goal_area,
-                opponents_in_shot_path=opponents_in_shot_path,
-                is_penalty=is_penalty,
-                is_open_goal=is_open_goal,
-                statsbomb_xg=statsbomb_xg,
-                outcome=outcome,
-                is_goal=is_goal))
+    # Shot Outcome
+    statsbomb_xg <- shot$shot$statsbomb_xg
+    outcome      <- shot$shot$outcome$name
+    is_goal      <- ifelse(shot$shot$outcome$id == 97, 1, 0)
+    
+    return(list(
+        competition = competition_name,
+        season = season,
+        match_id = match_id,
+        timestamp = timestamp,
+        period = period,
+        attacking_team = attacking_team,
+        defending_team = defending_team,
+        is_home = is_home,
+        shooter_position = shooter_position,
+        shot_dist = shot_dist,
+        dist_to_goal = dist_to_goal,
+        shot_angle = shot_angle,
+        body_part = body_part,
+        shot_technique = shot_technique,
+        shot_type = shot_type,
+        is_ca = is_ca,
+        is_penalty = is_penalty,
+        is_open_goal = is_open_goal,
+        teammates_in_frame = teammates_in_frame,
+        opponents_in_frame = opponents_in_frame,
+        closest_opponent_dist = closest_opponent_dist,
+        goalkeeper_dist = goalkeeper_dist,
+        opponents_in_penalty_area = opponents_in_penalty_area,
+        opponents_in_goal_area = opponents_in_goal_area,
+        opponents_in_shot_path = opponents_in_shot_path,
+        statsbomb_xg = statsbomb_xg,
+        outcome = outcome,
+        is_goal = is_goal
+    ))
 }
