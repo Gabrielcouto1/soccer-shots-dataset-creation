8cc94cfcd8245ad331c7936437662e29b4efaba9
Gabrielcouto1 <gabrielcouto3.14@gmail.com>
2025-07-01 15:55:29 -0300

    fix: wrong variable name fixed (god maintained it working while being wrong.)

--- r/create_dataset.r
+++ r/create_dataset.r
@@ -4,5 +4,5 @@ source("./r/utils/check_libraries.r")
 check_libraries()
 
 competitions <- c("bundesliga", "la_liga", "ligue_1", "premier_league", "serie_a")
-create_csvs(competition)
+create_csvs(competitions)
     
